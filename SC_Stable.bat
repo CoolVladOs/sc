bea00455b704deef380e4a16cbad3d8ce20532a1
CoolVladOs <vladospitatoshka@gmail.com>
2025-09-20 21:41:00 +0400

    Update SC_Stable.bat

--- SC_Stable.bat
+++ SC_Stable.bat
@@ -7,8 +7,105 @@ echo Please allow administrator previleges!
 cd /d "%~dp0" && ( if exist "%temp%\getadmin.vbs" del "%temp%\getadmin.vbs" ) && fsutil dirty query %systemdrive% 1>nul 2>nul || (  cmd /u /c echo Set UAC = CreateObject^("Shell.Application"^) : UAC.ShellExecute "cmd.exe", "/k cd ""%~sdp0"" && ""%~s0"" %Apply%", "", "runas", 1 >> "%temp%\getadmin.vbs" && "%temp%\getadmin.vbs" && echo [LOG] [%DATE%, %TIME%] Cannot get administrator access, exiting... >> C:\CoolVladOs\temp\sc4console\LOG.TXT && exit /B )
 echo Initializing...
 echo [LOG] [%DATE%, %TIME%] Detected administrator access! >> C:\CoolVladOs\temp\sc4console\LOG.TXT
+
+
+set version=5.1
+
+
+echo [LOG] [%DATE%, %TIME%] Version: 5.1 >> C:\CoolVladOs\temp\sc4console\LOG.TXT
 chcp 65001
-title SuperCleaner v5.1 (Console Version)
+title SuperCleaner v%version% (Console Version)
+: initsettings
+echo Checking FirstStartup setting...
+echo [LOG] [%DATE%, %TIME%] Checking FirstStartup setting... >> C:\CoolVladOs\temp\sc4console\LOG.TXT
+setlocal enabledelayedexpansion
+
+set "target_file=C:\CoolVladOs\AppSetting\SuperCleaner\StartSetting.ini"
+set "search_string=FirstStartup=1"
+
+if not exist "%target_file%" (
+    echo Файл настройки не найден!
+    echo [LOG] [%DATE%, %TIME%] Settings file not found! >> C:\CoolVladOs\temp\sc4console\LOG.TXT
+    goto settingcreate
+)
+
+findstr /C:"%search_string%" "%target_file%" >nul
+if !errorlevel! equ 0 (
+    echo [LOG] [%DATE%, %TIME%] FirstStartup setting is "1"! >> C:\CoolVladOs\temp\sc4console\LOG.TXT
+    goto initcomplete
+) else (
+    echo [LOG] [%DATE%, %TIME%] FirstStartup setting is "0" >> C:\CoolVladOs\temp\sc4console\LOG.TXT
+    goto firstsetup
+)
+
+
+: settingcreate
+echo Creating Settings file...
+echo [LOG] [%DATE%, %TIME%] Creating Settings file... >> C:\CoolVladOs\temp\sc4console\LOG.TXT
+cd C:\
+mkdir CoolVladOs
+cd CoolVladOs
+mkdir AppSetting
+cd AppSetting
+mkdir SuperCleaner
+cd SuperCleaner
+echo [author femboy] >> C:\CoolVladOs\AppSetting\SuperCleaner\StartSetting.ini
+echo. >> C:\CoolVladOs\AppSetting\SuperCleaner\StartSetting.ini
+echo [LOG] [%DATE%, %TIME%] File created! >> C:\CoolVladOs\temp\sc4console\LOG.TXT
+echo Settings file created!
+goto initsettings
+
+: firstsetup
+echo [LOG] [%DATE%, %TIME%] Starting first setup... >> C:\CoolVladOs\temp\sc4console\LOG.TXT
+echo FirstStartup=1 >> C:\CoolVladOs\AppSetting\SuperCleaner\StartSetting.ini
+cls
+echo Добро пожаловать в консольную версию SuperCleaner!
+echo.
+echo Так как вы впервый раз запускаете новую версию SuperCleaner, нам необходимо провести первоначальную настройку.
+echo После нажатия любой клавиши начнется первоначальная настройка.
+echo.
+echo.
+echo ⚠️⚠️ИМЕЙТЕ ВВИДУ⚠️⚠️
+echo Если в прошлой версии у вас была включена автоматическая очистка при запуске компьютера, то вам будет нужно включить её заново.
+echo.
+echo.
+pause
+cls
+echo Идёт первоначальная настройка...
+echo.
+echo Ищем ненужные файлы от старых версий...
+cd %AppData%
+cd Microsoft
+cd Windows
+cd "Start Menu"
+cd Programs
+cd Startup
+echo Делаем вас фурри фембойчиком...
+del /q /f SuperCleaner_AutorunScript.exe
+del /q /f SuperCleaner_AutorunScript.bat
+echo ой извини перепутал
+cls
+echo Идёт первоначальная настройка...
+echo.
+echo Ищем ненужные файлы от старых версий...
+echo Удаляем ненужные файлы от старых версий...
+del /q /f SuperCleaner_Autorun.exe
+del /q /f SuperCleaner_Autorun.bat
+del /q /f tempclear.bat
+del /q /f recentclear.bat
+cls
+echo Файлы удалены!
+echo [LOG] [%DATE%, %TIME%] First setup completed! >> C:\CoolVladOs\temp\sc4console\LOG.TXT
+echo теперь вы фурри фембойчик!
+cls 
+echo Файлы удалены!
+echo Возвращаем вас на этап инициализации...
+goto initsettings
+
+
+
+
+: initcomplete
 echo Done!
 cls
 echo Пожалуйста подождите...
@@ -117,7 +214,7 @@ goto modif
 : about
 echo [LOG] [%DATE%, %TIME%] Going to ABOUT >> C:\CoolVladOs\temp\sc4console\LOG.TXT
 cls
-echo SuperCleaner v5.1 (Console Version)
+echo SuperCleaner v%version% (Console Version)
 echo Maded By CoolVladOs
 echo.
 echo https://t.me/CoolVladOs
@@ -125,6 +222,7 @@ echo.
 echo ----------------
 echo.
 echo GitHub Page: https://github.com/CoolVladOs/SuperCleaner.git
+echo Telegram channel: https://t.me/SuperCleanerUpdate
 echo.
 echo.
 pause
@@ -184,6 +282,11 @@ if %var%== goto clean
 cls
 echo [LOG] [%DATE%, %TIME%] Started FULL Clean >> C:\CoolVladOs\temp\sc4console\LOG.TXT
 echo Подождите...
+cls
+echo Идёт полная очистка временных файлов...
+echo Это может занимать много времени. (от 1 до 30 минут)
+echo.
+echo Пожалуйста, наберитесь терпения и подождите конца очистки, по завершению вас переместит в главное меню.
 del /q/f/s "%HOMEPATH%\Local Settings\Temp"
 del /q/f/s %windir%\Logs
 del /q /f /s C:\Windows\debug
@@ -211,6 +314,20 @@ del /q /f /s C:\Windows\assembly\temp
 del /q /f /s C:\Windows\assembly\tmp
 del /q /f /s C:\Windows\assembly\NativeImages_v4.0.30319_64\Temp
 del /q /f /s C:\Windows\assembly\NativeImages_v4.0.30319_32\Temp
+del /q /f /s %localappdata%\NVIDIA\DXCache
+del /q /f /s %localappdata%\NVIDIA\GLCache
+del /q /f /s %localappdata%\numba\Cache
+del /q /f /s %localappdata%\D3DSCache
+del /q /f /s %localappdata%\EscapeTheBackrooms\Saved\Logs
+del /q /f /s %localappdata%\HD-MultiInstanceManager\cache
+del /q /f %localappdata%\labymodlauncher\Squirrel-CheckForUpdate.log
+del /q /f /s %localappdata%\lunarclient-updater
+del /q /f /s %localappdata%\max\cache
+del /q /f /s %localappdata%\Movavi\Logs
+del /q /f /s %localappdata%\ONEME\.crash_dumps
+del /q /f /s "%localappdata%\Opera Software\Opera Stable\Default\Cache"
+del /q /f /s "%localappdata%\Package Cache"
+del /q /f /s %localappdata%\Pekora\Downloads
 del /q /f /s C:\Windows\assembly\NativeImages_v2.0.50727_64\Temp
 del /q /f /s C:\Windows\assembly\NativeImages_v2.0.50727_32\Temp
 del /q /f /s C:\Windows\Performance\WinSAT
@@ -300,6 +417,90 @@ del /q /f /s C:\ProgramData\Microsoft\Windows\WER\Temp
 del /q /f /s C:\ProgramData\Microsoft\Windows\WindowsApps\Microsoft.GamingServices_29.103.2001.0_x64__8wekyb3d8bbwe\Cache
 del /q /f /s C:\ProgramData\Microsoft\Windows\WindowsApps\Microsoft.GamingServices*\Cache
 del /q /f /s "C:\ProgramData\Package Cache"
+del /q /f /s %localappdata%\Packages\ROBLOXCORPORATION.ROBLOX_55nm5eh3cm0pr\AC\Microsoft\CryptnetFlushCache
+del /q /f /s %localappdata%\Packages\ROBLOXCORPORATION.ROBLOX_55nm5eh3cm0pr\AC\Microsoft\CryptnetUrlCache
+del /q /f /s %localappdata%\Packages\ROBLOXCORPORATION.ROBLOX_55nm5eh3cm0pr\AC\Temp
+del /q /f "%localappdata%\Packages\ROBLOXCORPORATION.ROBLOX_55nm5eh3cm0pr\AppData\Indexed DB\edb.log"
+del /q /f "%localappdata%\Packages\ROBLOXCORPORATION.ROBLOX_55nm5eh3cm0pr\AppData\Indexed DB\edb00001.log"
+del /q /f "%localappdata%\Packages\ROBLOXCORPORATION.ROBLOX_55nm5eh3cm0pr\AppData\Indexed DB\edb*"
+del /q /f "%localappdata%\Packages\ROBLOXCORPORATION.ROBLOX_55nm5eh3cm0pr\AppData\Indexed DB\edbtmp.log"
+del /q /f /s %localappdata%\Packages\ROBLOXCORPORATION.ROBLOX_55nm5eh3cm0pr\AppData\CacheStorage
+del /q /f /s %localappdata%\Packages\ROBLOXCORPORATION.ROBLOX*\AC\Microsoft\CryptnetFlushCache
+del /q /f /s %localappdata%\Packages\ROBLOXCORPORATION.ROBLOX*\AC\Microsoft\CryptnetUrlCache
+del /q /f /s %localappdata%\Packages\ROBLOXCORPORATION.ROBLOX*\AC\Temp
+del /q /f "%localappdata%\Packages\ROBLOXCORPORATION.ROBLOX*\AppData\Indexed DB\edb.log"
+del /q /f "%localappdata%\Packages\ROBLOXCORPORATION.ROBLOX*\AppData\Indexed DB\edb00001.log"
+del /q /f "%localappdata%\Packages\ROBLOXCORPORATION.ROBLOX*\AppData\Indexed DB\edb*"
+del /q /f "%localappdata%\Packages\ROBLOXCORPORATION.ROBLOX*\AppData\Indexed DB\edbtmp.log"
+del /q /f /s %localappdata%\Packages\ROBLOXCORPORATION.ROBLOX*\AppData\CacheStorage
+del /q /f /s %localappdata%\Packages\ROBLOXCORPORATION.ROBLOX_55nm5eh3cm0pr\LocalCache
+del /q /f /s %localappdata%\Packages\ROBLOXCORPORATION.ROBLOX_55nm5eh3cm0pr\TempState
+del /q /f /s %localappdata%\Packages\ROBLOXCORPORATION.ROBLOX*\LocalCache
+del /q /f /s %localappdata%\Packages\ROBLOXCORPORATION.ROBLOX*\TempState
+del /q /f /s %localappdata%\Packages\*\AC\Microsoft\CryptnetFlushCache
+del /q /f /s %localappdata%\Packages\*\AC\Microsoft\CryptnetUrlCache
+del /q /f /s %localappdata%\Packages\*\AC\Temp
+del /q /f "%localappdata%\Packages\*\AppData\Indexed DB\edb.log"
+del /q /f "%localappdata%\Packages\*\AppData\Indexed DB\edb00001.log"
+del /q /f "%localappdata%\Packages\*\AppData\Indexed DB\edb*"
+del /q /f "%localappdata%\Packages\*\AppData\Indexed DB\edbtmp.log"
+del /q /f /s %localappdata%\Packages\*\AppData\CacheStorage
+del /q /f /s %localappdata%\Packages\*\LocalCache
+del /q /f /s %localappdata%\Packages\*\TempState
+del /q /f /s %localappdata%\D3DSCache
+del /q /f /s %localappdata%\Roblox\logs
+del /q /f /s %appdata%\Roblox\logs
+del /q /f /s %localappdata%\Roblox\rbx-storage
+del /q /f /s %appdata%\Roblox\rbx-storage
+del /q /f /s %localappdata%\Steam\htmlcache
+del /q /f /s %appdata%\Steam\htmlcache
+del /q /f /s %USERPROFILE%\vmlogs
+del /q /f /s %USERPROFILE%\.DevelNext\cache
+del /q /f /s %USERPROFILE%\.DevelNext\tmp
+del /q /f /s %USERPROFILE%\.DevelNext\log
+del /q /f /s %USERPROFILE%\.config\orb\logs
+del /q /f /s %USERPROFILE%\.gradle\caches
+del /q /f /s %USERPROFILE%\.gradle\.tmp
+del /q /f /s %USERPROFILE%\.Ld9VirtualBox
+del /q /f /s %USERPROFILE%\.lunarclient\game-cache
+del /q /f /s %USERPROFILE%\.lunarclient\launcher-cache
+del /q /f /s %USERPROFILE%\.lunarclient\logs
+del /q /f /s %USERPROFILE%\.lunarclient\mx-cache
+del /q /f /s %USERPROFILE%\.lunarclient\offline\multiver
+del /q /f /s %USERPROFILE%\.lunarclient\offline\multiver\cache
+del /q /f /s %USERPROFILE%\.lunarclient\offline\multiver\logs
+del /q /f /s %USERPROFILE%\LGP\FarlightOfficial\log
+del /q /f /s %USERPROFILE%\LGP\FarlightOfficial\alilog
+del /q /f /s C:\Intel\Logs
+del /q /f /s C:\Program Files\Google\Chrome\Application\138.0.7204.184\Installer
+del /q/f/s %USERPROFILE%\AppData\Local\Packages\MicrosoftWindows.Client.CBS_cw5n1h2txyewy\TempState
+del /q /f /s "%LOCALAPPDATA%\Google\Chrome\User Data\Profile 1\Cache"
+del /q /f /s "%LOCALAPPDATA%\Google\Chrome\User Data\Profile 2\Cache"
+del /q /f /s "%LOCALAPPDATA%\Google\Chrome\User Data\Profile 3\Cache"
+del /q /f /s "%LOCALAPPDATA%\Google\Chrome\User Data\Profile 4\Cache"
+del /q /f /s "%LOCALAPPDATA%\Google\Chrome\User Data\Profile 5\Cache"
+del /q /f /s "%LOCALAPPDATA%\Google\Chrome\User Data\Profile 6\Cache"
+del /q /f /s "%LOCALAPPDATA%\Google\Chrome\User Data\Profile 7\Cache"
+del /q /f /s "C:\MSOCache\All Users"
+del /q /f /s "C:\Program Files (x86)\Temp"
+del /q /f /s "C:\Program Files\Temp"
+del /q /f /s "C:\Program Files (x86)\Microsoft\Edge\Application\138.0.3351.121\Installer"
+del /q /f /s "C:\Program Files\Microsoft\Edge\Application\138.0.3351.121\Installer"
+del /q /f /s "C:\Program Files (x86)\Microsoft\Temp"
+del /q /f /s "C:\Program Files\Microsoft\Temp"
+del /q /f /s C:\ProgramData\Oracle\Java\installcache_x64
+del /q /f /s C:\ProgramData\Oracle\Java\installcache
+del /q /f /s C:\ProgramData\Oracle\Java\installcache_x86
+del /q /f /s C:\ProgramData\Roblox\Downloads
+del /q /f /s C:\$SysReset\Logs
+del /q /f /s "%LOCALAPPDATA%\Google\Chrome\User Data\Profile 8\Cache"
+del /q /f /s C:\Windows\SystemTemp
+del /q /f /s "%LOCALAPPDATA%\Google\Chrome\User Data\Profile 9\Cache"
+del /q /f /s "%LOCALAPPDATA%\Google\Chrome\User Data\Profile 10\Cache"
+del /q/f/s "%USERPROFILE%\AppData\Local\Packages\MicrosoftWindows.Client.WebExperience_cw5n1h2txyewy\LocalState\EBWebView\Default\Code Cache\js"
+del /q /f /s C:\Windows\security\logs
+del /q /f /s C:\Windows\System32\DriverStore\FileRepository
+del /q /f /s %USERPROFILE%\OpenVPN\log
 del /q /f /s C:\ProgramData\Packages\Microsoft*\*\SystemAppData\Helium\Cache
 del /q /f /s "C:\Program Files (x86)\Google\GoogleUpdater\crx_cache"
 del /q /f /s "C:\Program Files\Google\GoogleUpdater\crx_cache"
@@ -333,38 +534,69 @@ del /q/f/s %AppData%\vlc\art
 del /q/f/s %USERPROFILE%\AppData\Local\TeamViewer\EdgeBrowserControl\Persistent\518004909B1945429DCFDF9727D8D545\EBWebView\Default\Cache\Cache_Data
 del /q/f/s %USERPROFILE%\AppData\Local\TeamViewer\BuddyListCache
 del /q /f /s "C:\ProgramData\NVIDIA Corporation\Downloader"
+del /q /f /s C:\ProgramData\Acronis\SnapAPILogs
+del /q /f /s C:\ProgramData\Adobe\Installer
+del /q /f /s C:\ProgramData\AmneziaVPN\log
+del /q /f /s C:\ProgramData\AnyDesk\cache
+del /q /f C:\ProgramData\AnyDesk\ad_svc.trace
+del /q /f C:\ProgramData\AnyDesk\connection_trace.txt
+del /q /f C:\ProgramData\AnyDesk\file_transfer_trace.txt
+del /q /f /s "C:\ProgramData\Apple\Installer Cache"
+del /q /f /s C:\ProgramData\BlueStacks_nxt\Logs
+del /q /f /s C:\ProgramData\BlueStacks_nxt\Dumps
+del /q /f /s C:\ProgramData\LGHUB\cache
+del /q /f /s C:\ProgramData\LogMeIn\Dumps
+del /q /f /s C:\ProgramData\Microsoft\EdgeUpdate\Log
+del /q /f /s "C:\ProgramData\Microsoft\Windows Security Health\Logs"
+del /q /f C:\ProgramData\NVIDIA\*.log
+del /q /f C:\ProgramData\NVIDIA\*.log_backup1
+del /q /f /s "C:\ProgramData\NVIDIA Corporation\DisplayDriverRAS\NvTelemetry"
+del /q /f /s C:\ProgramData\obs-studio\shader-cache
+del /q /f /s C:\ProgramData\Oracle\Java\installcache_x64
+del /q /f /s C:\ProgramData\Oracle\Java\installcache
+del /q /f /s "C:\ProgramData\Package Cache"
+del /q /f /s C:\ProgramData\Packages\*\*\SystemAppData\Helium\Cache
+del /q /f /s "C:\ProgramData\PopCap Games\PlantsVsZombies\cached"
+del /q /f /s C:\ProgramData\Razer\Installer\Logs
+del /q /f /s C:\ProgramData\Razer\GameManager3\Logs
+del /q /f /s C:\ProgramData\Razer\RazerAppEngine\Logs
+del /q /f /s C:\ProgramData\Razer\*\Logs
+del /q /f /s "C:\ProgramData\Samsung\Device Error Recovery"
+del /q /f /s C:\ProgramData\temp
+for /d %%i in ("%localappdata%\Packages\*") do (
+del /q /f /s "%%i\AC\Microsoft\CryptnetFlushCache\*.*"
+del /q /f /s "%%i\AC\Microsoft\CryptnetUrlCache\*.*"
+del /q /f /s "%%i\AC\Temp\*.*"
+del /q /f /s "%%i\AppData\Indexed DB\edb.log\*.*"
+del /q /f /s "%%i\AppData\Indexed DB\edb00001.log\*.*"
+del /q /f /s "%%i\Indexed DB\edb*.*"
+del /q /f /s "%%i\AppData\CacheStorage\*.*"
+del /q /f /s "%%i\LocalCache\*.*"
+del /q /f /s "%%i\TempState\*.*"
+del /q /f /s "%%i\temp\*.*"
+del /q /f /s "%%i\temp\*.*"
+del /q /f /s "%%i\temp\*.*"
+del /q /f /s "%%i\temp\*.*"
+del /q /f /s "%%i\temp\*.*"
+del /q /f /s "%%i\temp\*.*"
+del /q /f /s "%%i\temp\*.*"
+del /q /f /s "%%i\temp\*.*"
+)
+for /d %%i in ("C:\Program Files\Google\Chrome\Application\*") do (
+del /q /f /s "%%i\Installer\*.*"
+)
+for /d /r C:\ %%i in (temp, cache, temporary, tmp, log, logs) do (
+    del /q /f /s "%%i"
+)
+for /d /r C:\ %%i in (*temp*, *cache*, *temporary*, *tmp*, *log*, *logs*) do (
+    del /q /f /s "%%i"
+)
+for /d /r C:\ %%i in (*temp*.*, *cache*.*, *temporary*.*, *tmp*.*, *log*.*, *logs*.*) do (
+    del /q /f /s "%%i"
+)
 cls
 echo Пожалуйста подождите...
 cleanmgr /autoclean
-del /q /f /s C:\Intel\Logs
-del /q /f /s C:\Program Files\Google\Chrome\Application\138.0.7204.184\Installer
-del /q/f/s %USERPROFILE%\AppData\Local\Packages\MicrosoftWindows.Client.CBS_cw5n1h2txyewy\TempState
-del /q /f /s "%LOCALAPPDATA%\Google\Chrome\User Data\Profile 1\Cache"
-del /q /f /s "%LOCALAPPDATA%\Google\Chrome\User Data\Profile 2\Cache"
-del /q /f /s "%LOCALAPPDATA%\Google\Chrome\User Data\Profile 3\Cache"
-del /q /f /s "%LOCALAPPDATA%\Google\Chrome\User Data\Profile 4\Cache"
-del /q /f /s "%LOCALAPPDATA%\Google\Chrome\User Data\Profile 5\Cache"
-del /q /f /s "%LOCALAPPDATA%\Google\Chrome\User Data\Profile 6\Cache"
-del /q /f /s "%LOCALAPPDATA%\Google\Chrome\User Data\Profile 7\Cache"
-del /q /f /s "C:\MSOCache\All Users"
-del /q /f /s "C:\Program Files (x86)\Temp"
-del /q /f /s "C:\Program Files\Temp"
-del /q /f /s "C:\Program Files (x86)\Microsoft\Edge\Application\138.0.3351.121\Installer"
-del /q /f /s "C:\Program Files\Microsoft\Edge\Application\138.0.3351.121\Installer"
-del /q /f /s "C:\Program Files (x86)\Microsoft\Temp"
-del /q /f /s "C:\Program Files\Microsoft\Temp"
-del /q /f /s C:\ProgramData\Oracle\Java\installcache_x64
-del /q /f /s C:\ProgramData\Oracle\Java\installcache
-del /q /f /s C:\ProgramData\Oracle\Java\installcache_x86
-del /q /f /s C:\ProgramData\Roblox\Downloads
-del /q /f /s C:\$SysReset\Logs
-del /q /f /s "%LOCALAPPDATA%\Google\Chrome\User Data\Profile 8\Cache"
-del /q /f /s C:\Windows\SystemTemp
-del /q /f /s "%LOCALAPPDATA%\Google\Chrome\User Data\Profile 9\Cache"
-del /q /f /s "%LOCALAPPDATA%\Google\Chrome\User Data\Profile 10\Cache"
-del /q/f/s "%USERPROFILE%\AppData\Local\Packages\MicrosoftWindows.Client.WebExperience_cw5n1h2txyewy\LocalState\EBWebView\Default\Code Cache\js"
-del /q /f /s C:\Windows\security\logs
-del /q /f /s C:\Windows\System32\DriverStore\FileRepository
 cls 
 echo Операция успешно завершена. (1/2)
 echo [LOG] [%DATE%, %TIME%] Operation Completed (1/1) >> C:\CoolVladOs\temp\sc4console\LOG.TXT
